e853eaf2dc4bb0fe42d0df3edf0c5a16a1c7b53b
Devin Lee <35930502+devintlee21@users.noreply.github.com>
2018-05-20 18:16:15 -0500

    c# web application

---
+++ ReverseStringWeb/reverseString.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="reverseString.aspx.cs" Inherits="ReverseStringWeb.reverseString" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<link href="~/main.css" rel="stylesheet" type="text/css" media="screen" runat="server" />
+    <title></title>
+	<style type="text/css">
+	</style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div Class="color">
+			 Enter a string to reverse!
+
+        </div>
+    	<p>
+			<asp:TextBox ID="reversedInput" runat="server" OnTextChanged="reversedInput_TextChanged" Width="175px"></asp:TextBox>
+        	<asp:Button ID="Button1" runat="server" Text="Reverse" OnClick="Button1_Click" />
+        </p>
+    	<p>
+
+			<asp:Label ID="reversedOutput" runat="server" Text="" CssClass="newStyle1"></asp:Label>
+		</p>
+    </form>
+</body>
+</html>
---
+++ ReverseStringWeb/reverseString.aspx.cs
@@ -0,0 +1,40 @@
+﻿/* Devin Lee 5/18/2018
+ * In this web application a user can input a string they want to reverse
+ * After pressing the button the inputed string will be reversed and displayed
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ReverseStringWeb
+{
+
+    public partial class reverseString: System.Web.UI.Page
+    {
+        public static string ReverseString(string r) //method that takes in a string and reverses it using a char array
+        {
+            int position = 0; //placeholder variable
+            char[] reversed = new char[r.Length]; //new char array to store string
+            for (int i = r.Length - 1; i >= 0; i--) //loop through the string at each char and place each char in reverse order in the array
+            {
+                reversed[position++] = r[i]; //char at current i = position in array
+            }
+            return new string(reversed); //output the new reversed string 
+        }
+
+
+        protected void Page_Load(object sender, EventArgs e){
+        }
+
+        protected void reversedInput_TextChanged(object sender, EventArgs e){
+        }
+
+        protected void Button1_Click(object sender, EventArgs e){
+            this.reversedOutput.Text = ReverseString(this.reversedInput.Text); //setting the output text label to the string input from user that was reversed using the reverseString method
+        }
+    }
+}
